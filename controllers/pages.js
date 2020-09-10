6165aa6cb68fc61bf42bdadc592a6a9056a19a45
jripke74 <jripke74@gmail.com>
2020-09-10 10:00:01 -0700

    add main.css and updated nav

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,5 +1,6 @@
 exports.getIndex = (reg, res, next) => {
   res.render('pages/index', {
-    pageTitle: 'Jeff Ripke | Portfolio'
+    pageTitle: 'Jeff Ripke | Portfolio',
+    path: '/home'
   });
 }
