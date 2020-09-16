afd89e7216d183e5e45a53d13ecc8e5444e839d8
jripke74 <jripke74@gmail.com>
2020-09-16 10:01:37 -0700

    working on list items

--- models/user.js
+++ models/user.js
@@ -5,14 +5,28 @@ const getDb = require('../utilities/database').getDb;
 const ObjectId = mongodb.ObjectID;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, list, id) {
     this.name = username;
     this.email = email;
+    this.list = list; // { list: [] }
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  }
+
+  addToList(photo) {
+    // const listPhoto = this.list.photos.findIndex(lp => {
+    //   return lp._id === photo._id;
+    // });
+    const updatedList = { items: [{ ...photo }]};
+    const db = getDb();
+    return db.collection('users').updateOne(
+      { _id: new ObjectId(this._id) },
+      { $set: { cart: updatedList } }
+    );
   }
 
   static findById(userId) {
